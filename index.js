64d898f70b995052287bccb0b07df6ab365b4299
harhil9705 <hlakhani9705@gmail.com>
2024-01-23 17:38:30 +0530

    not working

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
-const  {GatewayIntentBits,Client} = require("discord.js")
+const  Discord = require("discord.js")
 // const  {} = require("discord.js")
 
 try {
-    const client = new Client({ intents: [GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+    // const client = new Client({ intents: [GatewayIntentBits.Guilds,
+    //     GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
    
-   client.on("messageCreate",(message)=>{
+    const client = new Discord.Client({intents: ['Guilds' , 'GuildMessages' , 'MessageContent','GuildMembers']});
+
+//    client.on("messageCreate",(message)=>{
         //   message.reply({
         //     content:"bot"
         //   })
-        console.log(message.content);
-    })
+        // console.log(message.content);
+    // })
    
-   client.login("MTE5ODE0ODk3ODEwMTk4NTM2MQ.GUyCFX.d7l8mYfRngEzHihl_u0aI3EhpvEJ_7naP4NaqM")
+//    client.login("MTE5ODE0ODk3ODEwMTk4NTM2MQ.G949Li.DjRt2ulgceV2hPiNIiIC9YUPmZ2yEit_hAP9rc")
 } catch (error) {
     console.log(error.message);
 }
